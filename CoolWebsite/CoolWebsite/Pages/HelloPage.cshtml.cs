00a7cb6961acfd7713966345d78b20fd8d6c413f
Tintin Falk <tintinfalk01@gmail.com>
2024-05-27 13:04:49 +0200

    Added Hello-Page

---
+++ CoolWebsite/CoolWebsite/Pages/HelloPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoolWebsite.Pages
+{
+    public class HelloPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
